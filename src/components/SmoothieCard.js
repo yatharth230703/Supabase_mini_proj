6de71270e9711d7732bda6fa787aa4680d744f73
Yatharth <vasub0723@gmail.com>
2025-04-05 22:33:53 +0530

    added delete functionality along with delete button, yet to add refresh functionality

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,6 +1,21 @@
 import {Link} from 'react-router-dom'
 
 const SmoothieCard = ({smoothie}) =>{
+    
+    const handleDelete = async()=>{
+        const{data,error}= await supabase 
+          .from('smoothies')
+          .delete()
+          .eq('id',smoothie.id)
+
+        if (error) {
+            console.log(error)
+        }
+        if (data){
+            console.log(data)
+        }
+    }
+
     return (
         <div className="smoothie-card">
             <h3>{smoothie.title}</h3>
@@ -10,6 +25,8 @@ const SmoothieCard = ({smoothie}) =>{
                 <Link to={'/'+smoothie.id}>
                    <i className="material-icons">edit</i>
                 </Link>
+                <i className="material-icons" onClick={handleDelete}>delete</i>
+                {/*material-icons is a library of react that replaces keywords like edit, delete with actual icons, baad me dekhna if this can be customized or not*/}
             </div>
         </div>
     )
